0cb152a1e92e979ffa0beac1e21a0d5b718b2a68
Cory <nelson.cory@outlook.com>
2023-12-17 14:12:51 -0600

    Day 3 WIP

--- Day 3 - Gear Ratios.py
+++ Day 3 - Gear Ratios.py
@@ -9,6 +9,54 @@ def readFile():
     return fileData
 
 
+# clean data
+# need to preserve adjacency, no data can be removed
+def cleanData(fileData):
+    cleanData = []
+    splitData = fileData.split("\n")
+    for i in splitData:
+        cleanData.append(list(i))
+    return cleanData
+
+#return a list of part numbers which are adjacent to at least one symbol
+# adjacency may be diagonal
+def findPartNumbers(data):
+    #strategy - identify coordinates for numbers,
+    #           - sweep area around number to see if adjacent to a symbol
+    
+    partNumbers = []
+    for i in range(len(data)):
+        currNum = ""
+        arrNums = []
+        for j in range(len(data[0])): #assumes data has rectangular shape
+            if data[i][j].isnumeric():
+                currNum += data[i][j]
+                arrNums.append([i,j])
+            else:
+                if len(currNum > 0):
+                    #perform adjacency test for number
+                    
+                    # if adjacent to a symbol, add number to partNumbers
+                    pass
+                    currNum = ""
+                    arrNums = []
+    
+def determineAdjacency(data, numberList):
+    for element in numberList:
+        #check above (check an additional 1 left and right for diagonals)
+        
+        #check below  (check an additional 1 left and right for diagonals)
+        
+        #check left
+        
+        #check right
+        pass
+
 
 def main():
-	input()
+    fileData = readFile()
+    organizedData = cleanData(fileData)
+    #print(organizedData)
+    input()
+    
+main()
