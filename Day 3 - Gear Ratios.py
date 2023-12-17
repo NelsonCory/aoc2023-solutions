91abb350072d621765f8fbd3b43c7d056551c22e
Cory <nelson.cory@outlook.com>
2023-12-17 12:54:33 -0600

    Set Up gitignore

---
+++ Day 3 - Gear Ratios.py
@@ -0,0 +1,14 @@
+# Day 3: Gear Rations
+# Problem: https://adventofcode.com/2023/day/3
+#
+
+def readFile():
+    fileInput = open("input.txt")
+    fileData = fileInput.read()
+    fileInput.close()
+    return fileData
+
+
+
+def main():
+	input()
